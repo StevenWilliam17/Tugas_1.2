a87935902e1efb0e33e7f4edb46e6d866cde8127
Steven William <StevenWilliam_2226250019@mhs.mdp.ac.id>
2023-10-03 13:02:45 +0700

    Change to Tugas 2

--- bin/dart_application_if3b.dart
+++ bin/dart_application_if3b.dart
@@ -1,6 +1,15 @@
-import 'package:dart_application_if3b/dart_application_if3b.dart'
-    as dart_application_if3b;
+import 'persegiPanjang.dart';
 
-void main(List<String> arguments) {
-  print('Hello world');
+void main() {
+  int panjang = 5;
+  int lebar = 3;
+
+  PersegiPanjang persegi = PersegiPanjang(panjang, lebar);
+
+  int luas = persegi.hitungLuas();
+  int keliling = persegi.hitungKeliling();
+
+  print('Panjang Persegi Panjang : $panjang Lebar Persegi Panjang : $lebar');
+  print('Luas Persegi Panjang: $luas');
+  print('Keliling Persegi Panjang: $keliling');
 }
